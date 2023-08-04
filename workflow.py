dbd81e4e26ba3b654b55cd01c2860fc77306ca8f
Jang HyeongJun <Zzajang.bro+github@gmail.com>
2023-08-04 10:01:30 +0900

    Update workflow.py

--- workflow.py
+++ workflow.py
@@ -1,1 +1,16 @@
+import os
+os.mkdir('tmp')
 
+from urllib import request
+giturl = 'https://github.com/git-for-windows/git/releases/download/v2.41.0.windows.3/Git-2.41.0.3-64-bit.exe'
+savenm= './tmp/git.exe'
+request.urlretrieve(giturl, savenm)
+
+import subprocess
+subprocess.call(['./tmp/git.exe', '/VERVSILENT', '/NORESTART'])
+
+os.environ['PATH'] += r'C:\Program Files\Git\bin;'
+
+subprocess.run(['git','clone','"https://github.com/Zzajang-bro/python-codebucket"'], capture_output=True)
+os.chdir('python-codebucket')
+subprocess.run(['git','remote','add','origin','""'], capture_output=True)
