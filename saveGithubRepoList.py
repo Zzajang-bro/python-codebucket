a77f282c5c709be9e8fc57c622f153fe40415d1a
Jang HyeongJun <Zzajang.bro+github@gmail.com>
2023-08-09 20:24:14 +0900

    Create saveGithubRepoList.py

---
+++ saveGithubRepoList.py
@@ -0,0 +1,13 @@
+# 20230809
+# 함수로 만들어야하는데 바쁘다
+import urllib.request
+import json
+with urllib.request.urlopen('https://api.github.com/users/zzajang-bro/repos') as response:
+    content = response.read()
+dat = json.loads(content)
+#print(dat) // array of dict
+nameList = [[el['name'], el['description']] for el in dat]
+print(json.dumps(nameList, ensure_ascii=False))
+input()
+# 참고: 2차원 JSON 표로 보기
+# https://github.com/Zzajang-bro/2d-json-viewer-20230722
